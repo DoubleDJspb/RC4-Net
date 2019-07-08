731e5bc485214573f4a9555946fb5a4f69cbc9d2
DoubleDJspb <doubledj@mail.ru>
2019-07-08 10:14:10 +0300

    Rename

---
+++ RC4-Net/RC4CryptoServiceProvider.cs
@@ -0,0 +1,16 @@
+﻿using System.Security.Cryptography;
+
+namespace DD.Cryptography
+{
+    public sealed class RC4CryptoServiceProvider : RC4
+    {
+        /// <summary>
+        /// Initializes a new instance of the RC4CryptoServiceProvider class.
+        /// </summary>
+        public RC4CryptoServiceProvider()
+        {
+            // Change the limits for the current implementation (5 - 256 bytes).
+            LegalKeySizesValue = new KeySizes[] { new KeySizes(40, 2048, 0) };
+        }
+    }
+}
